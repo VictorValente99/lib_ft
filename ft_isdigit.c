ae1acb1f659854e5a97df76a92c8f91d170cee5b
Victor De Souza Valente <victde-s@c1r8p1.42sp.org.br>
2025-07-23 21:57:07 -0300

    test: ft_isdigit.c

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:00:43 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/22 18:01:25 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/07/23 21:17:28 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,22 @@ int	ft_isdigit(int c)
 	{
 		return (0);
 	}
-	return (0);
 }
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int	main(void)
+// {
+// 	printf("\n");
+// 	printf("A função isdigit, verifica se é um número e retorna 0 se for falso,\n");
+// 	printf("ou qualquer número != '0' se for verdadeiro.\n\n");
+// 	printf("-------- Original: %d\n", isdigit('1'));
+// 	printf("------------ Mine: %d\n", ft_isdigit('1'));
+// 	printf("-------- Original: %d\n", isdigit('a'));
+// 	printf("------------ Mine: %d\n", ft_isdigit('a'));
+// 	printf("-------- Original: %d\n", isdigit('!'));
+// 	printf("------------ Mine: %d\n", ft_isdigit('!'));
+// 	printf("\n");
+// 	return (0);
+// }
