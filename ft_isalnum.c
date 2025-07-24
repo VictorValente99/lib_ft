b8a690d9d1a29de16debedb9e225c3ed2bdf81bc
Victor De Souza Valente <victde-s@c1r8p1.42sp.org.br>
2025-07-23 21:49:32 -0300

    test: ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:01:59 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/22 18:02:28 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/07/23 21:17:40 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,21 @@ int	ft_isalnum(int c)
 		return (0);
 	}
 }
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int	main(void)
+// {
+// 	printf("\n");
+// 	printf("A função isalnum, verifica se é letra ou número\n");
+// 	printf("e retorna 0 se for falso, ou qualquer número != '0' se for verdadeiro.\n\n");
+// 	printf("-------- Original: %d\n", isalnum(1));
+// 	printf("------------ Mine: %d\n", ft_isalnum(1));
+// 	printf("-------- Original: %d\n", isalnum('a'));
+// 	printf("------------ Mine: %d\n", ft_isalnum('a'));
+// 	printf("-------- Original: %d\n", isalnum('!'));
+// 	printf("------------ Mine: %d\n", ft_isalnum('!'));
+// 	printf("\n");
+// 	return (0);
+// }
