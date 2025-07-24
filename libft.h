d5f6e2f3a94c105b0db906d31a5381c12c085d97
Victor De Souza Valente <victde-s@c1r8p1.42sp.org.br>
2025-07-23 21:58:38 -0300

    feat: libft.h

---
+++ libft.h
@@ -0,0 +1,185 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 16:39:08 by victde-s          #+#    #+#             */
+/*   Updated: 2025/07/23 21:47:51 by victde-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+//SEÇÃO DE CHAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+
+//SEÇÃO DE STRINGS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+int	ft_strlen(char *c);
+
+int	ft_strchr()
+{
+
+	return (0);
+}
+
+int	ft_strrchr()
+{
+
+	return (0);
+}
+
+int	ft_strncmp(char *str1, char *str2, int n);
+
+int	ft_strlcpy()
+{
+
+	return (0);
+}
+
+int	ft_strlcat()
+{
+
+	return (0);
+}
+
+int	ft_strdup()
+{
+
+	return (0);
+}
+
+int	ft_strnstr()
+{
+
+	return (0);
+}
+
+int	ft_strjoin()
+{
+
+	return (0);
+}
+
+int	ft_strtrim()
+{
+
+	return (0);
+}
+
+int	ft_split()
+{
+
+	return (0);
+}
+
+int	ft_substr()
+{
+
+	return (0);
+}
+
+int	ft_strmapi()
+{
+
+	return (0);
+}
+
+int	ft_striteri()
+{
+
+	return (0);
+}
+
+//SEÇÃO DE MEMÓRIA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+int	ft_memset()
+{
+
+	return (0);
+}
+
+int	ft_memcpy()
+{
+
+	return (0);
+}
+
+int	ft_memmove()
+{
+
+	return (0);
+}
+
+int	ft_memchr()
+{
+
+	return (0);
+}
+
+int	ft_memcmp()
+{
+
+	return (0);
+}
+
+int	ft_calloc()
+{
+
+	return (0);
+}
+
+int	ft_bzero()
+{
+
+	return (0);
+}
+
+int	ft_atoi()
+{
+
+	return (0);
+}
+
+int	ft_itoa()
+{
+
+	return (0);
+}
+
+//SEÇÃO DE SAÍDA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+int	ft_putchar_fd()
+{
+
+	return (0);
+}
+
+int	ft_putstr_fd()
+{
+
+	return (0);
+}
+
+int	ft_putendl_fd()
+{
+
+	return (0);
+}
+
+int	ft_putnbr_fd()
+{
+
+	return (0);
+}
+
+#endif
