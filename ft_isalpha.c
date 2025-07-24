6845773b322b751f42de76d4d18fe34038dfb525
Victor De Souza Valente <victde-s@c1r8p1.42sp.org.br>
2025-07-23 21:56:18 -0300

    test: ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:59:11 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/22 17:59:55 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/07/23 20:45:02 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,19 @@ int	ft_isalpha(int c)
 		return (0);
 	}
 }
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int	main(void)
+// {
+// 	printf("\n");
+// 	printf("A função isalpha, verifica se é contido de 'a' a 'z' ou de 'A' até 'Z'\n");
+// 	printf("e retorna 0 se for falso, ou qualquer número != '0' se for verdadeiro.\n\n");
+// 	printf("-------- Original: %d\n", isalpha(1));
+// 	printf("------------ Mine: %d\n", ft_isalpha(1));
+// 	printf("-------- Original: %d\n", isalpha('a'));
+// 	printf("------------ Mine: %d\n", ft_isalpha('a'));
+// printf("\n");
+// 	return (0);
+// }
