2b1ab872af96d93580c04828d2fb6e75dcc8f0f8
Victor De Souza Valente <victde-s@c1r8p1.42sp.org.br>
2025-07-23 22:20:59 -0300

    norm correction: ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:04:25 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/23 22:18:44 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/07/23 22:20:30 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	ft_strlen(char *c)
 	return (i);
 }
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 
-int	main(void)
-{
-	printf("A função strlen, percorre uma string, e retorna\n");
-	printf("a quantidade de char's que tem na str\n\n");
-	printf("-------- Original: %zu\n", strlen("123"));
-	printf("------------ Mine: %d\n\n", ft_strlen("123"));
-	return (0);
-}
+// int	main(void)
+// {
+// 	printf("A função strlen, percorre uma string, e retorna\n");
+// 	printf("a quantidade de char's que tem na str\n\n");
+// 	printf("-------- Original: %zu\n", strlen("123"));
+// 	printf("------------ Mine: %d\n\n", ft_strlen("123"));
+// 	return (0);
+// }
