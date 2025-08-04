7d9048b3782fd3910ba96f9a7f6efddf65815bc3
Victor De Souza Valente <victde-s@c3r6p1.42sp.org.br>
2025-08-04 15:56:58 -0300

    norm refactoring

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:02:47 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/23 21:17:33 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:41:06 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ int	ft_isascii(int c)
 // int	main(void)
 // {
 // 	printf("\n");
-// 	printf("A função isascii, verifica se é contido dentro da tabela ascii\n");
-// 	printf("e retorna 0 se for falso, ou qualquer número != '0' se for verdadeiro.\n\n");
+// 	printf("Isascii, verify if > ascii < \n");
+// 	printf("'0' if false, or != '0' if ==\n\n");
 // 	printf("-------- Original: %d\n", isascii(1));
 // 	printf("------------ Mine: %d\n", ft_isascii(1));
 // 	printf("-------- Original: %d\n", isascii('a'));
