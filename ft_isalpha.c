77a48e7036dd7002fe3aeb51f879285e70c3dfec
Victor De Souza Valente <victde-s@c3r6p1.42sp.org.br>
2025-08-04 15:56:40 -0300

    norm refactoring

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:59:11 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/23 20:45:02 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:39:51 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,8 @@ int	ft_isalpha(int c)
 // int	main(void)
 // {
 // 	printf("\n");
-// 	printf("A função isalpha, verifica se é contido de 'a' a 'z' ou de 'A' até 'Z'\n");
-// 	printf("e retorna 0 se for falso, ou qualquer número != '0' se for verdadeiro.\n\n");
+// 	printf("Isalpha, verify if 'a' && 'z' || 'A' && 'Z'\n");
+// 	printf("return '0' if false, or != '0' if ==\n\n");
 // 	printf("-------- Original: %d\n", isalpha(1));
 // 	printf("------------ Mine: %d\n", ft_isalpha(1));
 // 	printf("-------- Original: %d\n", isalpha('a'));
