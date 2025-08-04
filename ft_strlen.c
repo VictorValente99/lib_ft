a6d7e54079329b90ec26cbc1f802a5b524c48ff7
Victor De Souza Valente <victde-s@c3r6p1.42sp.org.br>
2025-08-04 15:57:45 -0300

    norm refactoring

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 18:04:25 by victde-s          #+#    #+#             */
-/*   Updated: 2025/07/23 22:20:30 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/08/04 15:45:03 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	ft_strlen(char *c)
 
 // int	main(void)
 // {
-// 	printf("A função strlen, percorre uma string, e retorna\n");
-// 	printf("a quantidade de char's que tem na str\n\n");
+// 	printf("Strlen, pass by a str, and return\n");
+// 	printf("the N char's on str.\n\n");
 // 	printf("-------- Original: %zu\n", strlen("123"));
 // 	printf("------------ Mine: %d\n\n", ft_strlen("123"));
 // 	return (0);
