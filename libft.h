ce262a793329a90db597e53fa52260a62eb6fff4
Victor De Souza Valente <victde-s@c1r7p1.42sp.org.br>
2025-08-31 19:37:57 -0300

    header update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 16:39:08 by victde-s          #+#    #+#             */
-/*   Updated: 2025/08/29 13:31:29 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/08/31 19:36:40 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	**ft_split(char const *s, char c);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
