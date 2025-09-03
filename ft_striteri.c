a8de4892c86958de396cb2c29e9fc2b6aabc2c87
Victor De Souza Valente <victde-s@c2r4p4.42sp.org.br>
2025-09-03 17:57:35 -0300

    all done

--- ft_striteri.c
+++ ft_striteri.c
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victde-s <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 01:13:27 by victde-s          #+#    #+#             */
-/*   Updated: 2025/08/29 01:13:28 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/09/03 17:54:07 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int	i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: victde-s <victde-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/27 22:40:01 by victde-s          #+#    #+#             */
-/*   Updated: 2025/08/29 00:56:41 by victde-s         ###   ########.fr       */
+/*   Updated: 2025/09/03 17:51:40 by victde-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,23 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	useful_start;
-	size_t	useful_end;
+	int		start;
+	int		end;
 	char	*new_str;
 
 	if (!s1 || !set)
 		return (NULL);
-	useful_start = 0;
-	useful_end = ft_strlen(s1) - 1;
-	while (s1[useful_start] && ft_strchr(set, s1[useful_start]))
-		useful_start++;
-	while (useful_end > useful_start && ft_strchr(set, s1[useful_end]))
-		useful_end--;
-	new_str = ft_calloc((useful_end - useful_start) + 2, sizeof(char));
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (start <= end && ft_strchr(set, s1[start]))
+		start++;
+	if (start > end)
+		return (ft_strdup(""));
+	while (ft_strchr(set, s1[end]) && end >= 0)
+		end--;
+	new_str = (char *)ft_calloc(end - start + 2, sizeof(char));
 	if (!new_str)
 		return (NULL);
-	ft_strlcpy(new_str, s1 + useful_start, (useful_end - useful_start) + 2);
+	ft_strlcpy(new_str, &s1[start], end - start + 2);
 	return (new_str);
 }
